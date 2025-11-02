02482bea461344fa61c0fe02e5aa45098b4c49b1
Asad Mansoor <7865693+asadmansr@users.noreply.github.com>
2025-11-02 10:24:10 -0500

    Added textfield to add steps

---
+++ app/src/main/java/com/asadmansr/stepwise/components/home/AddStep.kt
@@ -0,0 +1,105 @@
+package com.asadmansr.stepwise.components.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.InputTransformation
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.maxLength
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.foundation.text.input.then
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.core.text.isDigitsOnly
+import com.asadmansr.stepwise.ui.theme.DarkGray
+import com.asadmansr.stepwise.ui.theme.EmeraldGreen
+import com.asadmansr.stepwise.ui.theme.Graphite
+import com.asadmansr.stepwise.ui.theme.White
+
+@Composable
+fun AddStep() {
+    val focusRequester = remember { FocusRequester() }
+    val stepText = rememberTextFieldState()
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                color = Graphite,
+                shape = RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp)
+            )
+            .padding(24.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            TextField(
+                state = stepText,
+                lineLimits = TextFieldLineLimits.SingleLine,
+                label = { Text("Steps") },
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Graphite,
+                    unfocusedContainerColor = Graphite,
+                    focusedTextColor = White,
+                    unfocusedTextColor = White,
+                    focusedLabelColor = DarkGray,
+                    unfocusedLabelColor = DarkGray,
+                    cursorColor = White,
+                    focusedIndicatorColor = Graphite,
+                    unfocusedIndicatorColor = Graphite
+
+                ),
+                modifier = Modifier.focusRequester(focusRequester),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                inputTransformation = InputTransformation.maxLength(5).then {
+                    if (!this.asCharSequence().isDigitsOnly()) {
+                        revertAllChanges()
+                    }
+                },
+            )
+
+            IconButton(
+                colors = IconButtonDefaults.iconButtonColors(
+                    containerColor = EmeraldGreen,
+                    contentColor = White
+                ),
+                shape = RoundedCornerShape(8.dp),
+                onClick = { }
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.Check,
+                    contentDescription = "Add new step"
+                )
+            }
+
+            Spacer(modifier = Modifier
+                .fillMaxWidth()
+                .height(64.dp))
+        }
+    }
+}
--- app/src/main/java/com/asadmansr/stepwise/components/home/HomeScreen.kt
+++ app/src/main/java/com/asadmansr/stepwise/components/home/HomeScreen.kt
@@ -1,13 +1,26 @@
 package com.asadmansr.stepwise.components.home
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import com.asadmansr.stepwise.ui.theme.Graphite
+import com.asadmansr.stepwise.ui.theme.LightViolet
 import com.asadmansr.stepwise.ui.theme.OrangeBackground
 import com.asadmansr.stepwise.ui.theme.PinkBackground
 
@@ -15,19 +28,54 @@ import com.asadmansr.stepwise.ui.theme.PinkBackground
 fun HomeScreen(
     onSettingsTapped: () -> Unit = {}
 ) {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(
-                    Brush.linearGradient(
-                        listOf(OrangeBackground, PinkBackground)
-                    )
-                )
-                .padding(innerPadding)
-        ) {
-            HomeScreenHeader(onSettingsTapped)
-            HomeScreenBanner()
+    var isAddStepToggled by rememberSaveable { mutableStateOf(false) }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        floatingActionButton = {
+            if (!isAddStepToggled) {
+                FloatingActionButton(
+                    onClick = {
+                        isAddStepToggled = true
+                    },
+                    containerColor = LightViolet,
+                    contentColor = Graphite
+                ) {
+                    Icon(Icons.Rounded.Add, "Add new steps.")
+                }
+            }
         }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.linearGradient(
+                            listOf(OrangeBackground, PinkBackground)
+                        )
+                    )
+                    .clickable{
+                        isAddStepToggled = false
+                    }
+                    .padding(innerPadding)
+            ) {
+                HomeScreenHeader(onSettingsTapped)
+                HomeScreenBanner()
+            }
+
+            if (isAddStepToggled) {
+                Column(
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(bottom = innerPadding.calculateBottomPadding())
+                ) {
+                    AddStep()
+                }
+            }
+        }
+
     }
 }
